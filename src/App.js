3a9bbed607bf2384c5738b870a1d9b66180f4680
Francis Pegenia <francispegenia15@gmail.com>
2020-09-16 16:40:39 +0800

    Adding form

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Loan Application</h1>
+      <p>Enter Information for loan application</p>
+      <form className="loan-form">
+        <label for="fullName">Full Name:</label>
+        <input name="fullName" type="text" required></input>
+        <label for="phone">Contact</label>
+        <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"></input>
+        <label for="date">Date:</label>
+        <input type="date" id="birthday" name="date"></input>
+        <label for="amount">Amount:</label>
+        <input type="number" id="birthday" name="amount"></input>
+        <label for="interest">Deduc Interest</label>
+        <select for="interest">
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+        </select>
+      </form>
     </div>
   );
 }
