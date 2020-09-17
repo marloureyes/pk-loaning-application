d00a7b221bae288a3aebb71502d90560f6b81d21
Marlou Reyes <69040884+marloureyes@users.noreply.github.com>
2020-09-17 18:43:07 +0800

    add security for api

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,20 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-
+console.log(process.env.REACT_APP_FIREBASE_API_KEY)
+console.log(process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID)
+console.log(process.env.REACT_APP_FIREBASE_APP_ID)
+console.log(process.env.REACT_APP_FIREBASE_MEASUREMENT_ID)
 // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
-    apiKey: "AIzaSyCY3jTtvdHH9yEg2jYQI_VBLUjHwYZzceE",
+    apiKey: process.NODE_ENV === 'development' ? process.env.REACT_APP_FIREBASE_API_KEY : process.FIREBASE_API_KEY,
     authDomain: "pk-loan-app.firebaseapp.com",
     databaseURL: "https://pk-loan-app.firebaseio.com",
     projectId: "pk-loan-app",
     storageBucket: "pk-loan-app.appspot.com",
-    messagingSenderId: "860099458950",
-    appId: "1:860099458950:web:4e80d3ca6475f24e6d9e07",
-    measurementId: "G-ZRH2REPF9N"
+    messagingSenderId: process.NODE_ENV === 'development' ? process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID : process.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.NODE_ENV === 'development' ? process.env.REACT_APP_FIREBASE_APP_ID : process.FIREBASE_APP_ID,
+    measurementId: process.NODE_ENV === 'development' ? process.env.REACT_APP_FIREBASE_MEASUREMENT_ID: process.FIREBASE_APP_ID,
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
