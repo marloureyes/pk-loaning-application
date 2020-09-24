f2366a8e506ceeae4e902f0f26d4d0be2d815730
Marlou Reyes <69040884+marloureyes@users.noreply.github.com>
2020-09-25 02:01:25 +0800

    env

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import 'firebase/auth';
 // 12
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
-    apiKey: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_FIREBASE_API_KEY : process.FIREBASE_API_KEY,
+    apiKey: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_FIREBASE_API_KEY : process.env.FIREBASE_API_KEY,
     authDomain: "pk-loan-app.firebaseapp.com",
     databaseURL: "https://pk-loan-app.firebaseio.com",
     projectId: "pk-loan-app",
     storageBucket: "pk-loan-app.appspot.com",
-    messagingSenderId: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID : process.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_FIREBASE_APP_ID : process.FIREBASE_APP_ID,
-    measurementId: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_FIREBASE_MEASUREMENT_ID: process.FIREBASE_APP_ID,
+    messagingSenderId: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID : process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_FIREBASE_APP_ID : process.env.FIREBASE_APP_ID,
+    measurementId: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_APP_ID,
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
